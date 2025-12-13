dd2acf68a82ae24ed3eca401ddee7ec0b68f77c6
AmelyanchukVolha <522783@student.ue.wroc.pl>
2025-12-13 23:42:47 +0100

    Zmieniłam formularz. Pola Imię, Nazwisko, Adres e-mail, Numer telefonu

---
+++ main.py
@@ -0,0 +1,30 @@
+#Stwórz formularz do opisu książki w bazie danych (bez zapisu)
+import tkinter as tk
+from tkinter import messagebox
+root = tk.Tk()
+root.title("Rejestr wolontariusza")
+# Etykiety i pola tekstowe
+label_name = tk.Label(root, text="Imię:")
+label_name.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+entry_name = tk.Entry(root, width=40)
+entry_name.grid(row=0, column=1, padx=10, pady=5)
+
+label_surname = tk.Label(root, text="Nazwisko:")
+label_surname.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+entry_surname = tk.Entry(root, width=40)
+entry_surname.grid(row=1, column=1, padx=10, pady=5)
+
+label_email = tk.Label(root, text="Adres e-mail:")
+label_email.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+entry_email = tk.Entry(root, width=40)
+entry_email.grid(row=2, column=1, padx=10, pady=5)
+
+label_phonenumber = tk.Label(root, text="Numer telefonu:")
+label_phonenumber.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+entry_phonenumber = tk.Entry(root, width=40)
+entry_phonenumber.grid(row=3, column=1, padx=10, pady=5)
+# Przycisk zapisu
+button_save = tk.Button(root, text="Zapisz", command=lambda x:x)
+button_save.grid(row=4, column=0, columnspan=2, pady=10)
+# Uruchomienie pętli głównej
+root.mainloop()
