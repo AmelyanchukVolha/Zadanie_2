aa218905cb37b496de035d6abf18b2c7bfc204cd
522795 <zbanyszek.natalia@gmail.com>
2025-12-14 21:58:57 +0100

    Dodałam 3 wiersze: wykształcenie, doświadczenie wolontariackie, języki obce

--- main.py
+++ main.py
@@ -45,8 +45,23 @@ label_pesel.grid(row=7, column=0, padx=10, pady=5, sticky="w")
 entry_pesel = tk.Entry(root, width=40)
 entry_pesel.grid(row=7, column=1, padx=10, pady=5)
 
+label_education = tk.Label(root, text="Wykształcenie:")
+label_education.grid(row=8, column=0, padx=10, pady=5, sticky="w")
+entry_education = tk.Entry(root, width=40)
+entry_education.grid(row=8, column=1, padx=10, pady=5)
+
+label_voluneer_experience = tk.Label(root, text="Doświadczenie wolontariackie:")
+label_voluneer_experience.grid(row=9, column=0, padx=10, pady=5, sticky="w")
+entry_voluneer_experience = tk.Entry(root, width=40)
+entry_voluneer_experience.grid(row=9, column=1, padx=10, pady=5)
+
+label_foreign_language = tk.Label(root, text="Języki obce:")
+label_foreign_language.grid(row=10, column=0, padx=10, pady=5, sticky="w")
+entry_foreign_language = tk.Entry(root, width=40)
+entry_foreign_language.grid(row=10, column=1, padx=10, pady=5)
+
 # Przycisk zapisu
 button_save = tk.Button(root, text="Zapisz !!!", command=lambda x:x, )
-button_save.grid(row=10, column=0, columnspan=2, pady=10)
+button_save.grid(row=11, column=0, columnspan=2, pady=10)
 # Uruchomienie pętli głównej
 root.mainloop()
